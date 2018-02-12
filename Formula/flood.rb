fa07c6f6e95ac08ad7ba6810a434f661bc2aefd4
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-13 10:44:48 +1100

    flood version v0.2.3

--- Formula/flood.rb
+++ Formula/flood.rb
@@ -1,9 +1,9 @@
 class Flood < Formula
   desc "Flood IO Command Line Interface"
   homepage "https://github.com/flood-io/cli"
-  url "https://github.com/flood-io/cli/releases/download/v0.2.3/flood-0.2.3-darwin-amd64.zip"
+  url "https://github.com/flood-io/cli/releases/download/v0.2.3/flood-0.2.3-darwin-amd64.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   version "0.2.3"
-  sha256 "263f6eb28eb736e0f297bf012aaa7a170fd780b84625fea8f46fe2427ee3ebf8"
+  sha256 "cd2e83464e514935aa497262a8adab191d69f14f7058767dbbfc90692098a6cb"
 
   def install
     bin.install "flood"
