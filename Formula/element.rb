d2ec431240858ae2dd91cef382e24c4474ed413d
Lachie Cox <lachiec@gmail.com>
2018-08-22 08:22:16 +0700

    tap for element cli

---
+++ Formula/element.rb
@@ -0,0 +1,21 @@
+require "language/node"
+
+class Element < Formula
+  desc "Flood Element CLI"
+  homepage "https://github.com/flood-io/element"
+  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.3-25.tgz",
+  sha256 "e2ec8b13d59e6cb6184674479b42d83455833e52c43b11c6eaca7d87b6b698de"
+
+  depends_on "node"
+  # uncomment if there is a native addon inside the dependency tree
+  # depends_on "python" => :build
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    # add a meaningful test here
+  end
+end
