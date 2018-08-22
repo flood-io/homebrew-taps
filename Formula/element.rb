8b5affb33cb8a3cb608496d7163a20d67ec1e6cc
Lachie Cox <lachiec@gmail.com>
2018-08-22 12:21:15 +0700

    typo

--- Formula/element.rb
+++ Formula/element.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Element < Formula
   desc "Flood Element CLI"
   homepage "https://github.com/flood-io/element"
-  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.3-25.tgz",
+  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.3-25.tgz"
   sha256 "e2ec8b13d59e6cb6184674479b42d83455833e52c43b11c6eaca7d87b6b698de"
 
   depends_on "node"
