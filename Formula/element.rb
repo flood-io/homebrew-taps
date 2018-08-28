8f049ca510239367fab183821159a4610f216dfb
Lachie Cox <lachiec@gmail.com>
2018-08-29 09:40:06 +1000

    published element brew tap

--- Formula/element.rb
+++ Formula/element.rb
@@ -4,8 +4,8 @@ require "language/node"
 class Element < Formula
   desc "Flood Element CLI"
   homepage "https://github.com/flood-io/element"
-  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.3-35.tgz"
-  sha256 "fb9ed6f8642c3a3fb2616e092b9c18df422e10b36a8be4c2d6400dceb177370f"
+  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.3-36.tgz"
+  sha256 "dddff35a45044e1e6e26c02827b77957cd19db87a4f7d3026f90f02773894601"
 
   depends_on "node"
   # uncomment if there is a native addon inside the dependency tree
