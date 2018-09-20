19947a712f91326cebd78787f8d662fc07166c3a
flud-buildbox <accounts@flood.io>
2018-09-20 01:17:41 +0000

    published element brew tap

---
+++ Formula/element@0.0.2-beta.6.rb
@@ -0,0 +1,22 @@
+
+require "language/node"
+
+class ElementAT002Beta6 < Formula
+  desc "Flood Element CLI"
+  homepage "https://github.com/flood-io/element"
+  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-0.0.2-beta.6.tgz"
+  sha256 "3cff7805c994923d9eb6aff4ef9a6c6d91dfc879a00a8e87f8fc05b22f74d341"
+
+  depends_on "node"
+  # uncomment if there is a native addon inside the dependency tree
+  # depends_on "python" => :build
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    # add a meaningful test here
+  end
+end
