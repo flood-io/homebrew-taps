eeadcf74e28b9550d2f174359222b36683fc42e2
flud-buildbox <accounts@flood.io>
2018-11-09 11:07:57 +0000

    published element brew tap

--- Formula/element@1.0.2-beta.rb
+++ Formula/element@1.0.2-beta.rb
@@ -4,8 +4,8 @@ require "language/node"
 class ElementAT102Beta < Formula
   desc "Flood Element CLI"
   homepage "https://github.com/flood-io/element"
-  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.2-beta.4.tgz"
-  sha256 "7eabadc577d9baaf1ad2143fd7b7da411ad9a74e5d21cade50e3456bd6c506b9"
+  url "https://registry.npmjs.org/@flood/element-cli/-/element-cli-1.0.2-beta.5.tgz"
+  sha256 "f86648cdbd59cc5fcbecba7724cfcfc936f75d88b3d7e82b66798efa092aabd9"
 
   depends_on "node"
   # uncomment if there is a native addon inside the dependency tree
