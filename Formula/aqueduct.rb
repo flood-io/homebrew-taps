4e389fd468c7c035d903068763647ace1d3b9993
Lachie Cox <lachiec@gmail.com>
2019-02-20 14:08:34 +1100

    published aqueduct stable

---
+++ Formula/aqueduct.rb
@@ -0,0 +1,17 @@
+require "language/node"
+
+class Aqueduct < Formula
+  desc "Flood Aqueduct Client"
+  homepage "https://aqueduct.flood.io"
+  url "https://https://s3.amazonaws.com/flood-aqueduct/aqueduct/stable/aqueduct-stable-darwin-amd64"
+  version "stable"
+  sha256 "35bfa613fd5a69bab5ba849ffc295de67e46aa63589a11e22ce0eee9b0d96b0b"
+
+  def install
+    bin.install "aqueduct"
+  end
+
+  test do
+    system bin/"aqueduct", "version"
+  end
+end
