b40c0928af051a61adbd2a5cd827258ff23a6671
Bob the Builder <accounts@flood.io>
2019-12-04 07:43:24 +0000

    published aqueduct stable

--- Formula/aqueduct.rb
+++ Formula/aqueduct.rb
@@ -5,7 +5,7 @@ class Aqueduct < Formula
   homepage "https://aqueduct.flood.io"
   url "https://s3.amazonaws.com/flood-aqueduct/aqueduct/stable/aqueduct-stable-darwin-amd64"
   version "stable"
-  sha256 "098198dbc77921e4ace46247c2e43c2a23b75989d3c323c96577755e79bbdb32"
+  sha256 "7ab9e1f277ae6f796f16d82d7c7e5ef1a33fb9ca30b0c87006573442e6e87105"
 
   def install
     system "cp", "aqueduct-stable-darwin-amd64", "aqueduct"
